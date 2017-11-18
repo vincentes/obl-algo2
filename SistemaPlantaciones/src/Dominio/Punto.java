96520dc75bcc48a482e8ea7ce5b20f4d69a39c58
vincentes <stealyerms@gmail.com>
2017-11-18 15:05:47 -0200

    Arreglar unit test CiudadExistente

--- SistemaPlantaciones/src/Dominio/Punto.java
+++ SistemaPlantaciones/src/Dominio/Punto.java
@@ -35,8 +35,40 @@ public class Punto  implements Comparable<Object>{
 			return coordY.compareTo(((Punto)o).getCoordY());
 		}else{
 			return -1;
-		}
-		
+		}	
 	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((coordX == null) ? 0 : coordX.hashCode());
+		result = prime * result + ((coordY == null) ? 0 : coordY.hashCode());
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Punto other = (Punto) obj;
+		if (coordX == null) {
+			if (other.coordX != null)
+				return false;
+		} else if (!coordX.equals(other.coordX))
+			return false;
+		if (coordY == null) {
+			if (other.coordY != null)
+				return false;
+		} else if (!coordY.equals(other.coordY))
+			return false;
+		return true;
+	}
+	
+	
+	
 	
 }
